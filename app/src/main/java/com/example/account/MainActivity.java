ef6abd30eb0ab2269c7cbe824883224806a7a523
Hung. Minh Nguyen <89327966+holdennguyen@users.noreply.github.com>
2022-09-06 10:05:41 +0700

    init

---
+++ app/src/main/java/com/example/account/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String SAVE_PREF = "save_pref";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gotoLoginScreen();
+    }
+
+    public void gotoRegisterScreen() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_main, new M001RegisterFragment()).commit();
+    }
+
+    public void gotoLoginScreen() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_main, new M000LoginFragment()).commit();
+    }
+}
